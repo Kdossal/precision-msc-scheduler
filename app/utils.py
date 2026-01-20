362964412cb170c346a5e525d3f9076a80b7d5e1
Kameel Dossal <103609764+Kdossal@users.noreply.github.com>
2026-01-20 16:35:34 -0500

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,19 +1,73 @@
 time_slots = {
     "Tuesday, February 24th": {
         "8:00 AM": None,
+        "8:30 AM": None,
         "9:00 AM": None,
+        "9:30 AM": None,
         "10:00 AM": None,
+        "10:30 AM": None,
         "11:00 AM": None,
+        "11:30 AM": None,
         "12:00 PM": "LUNCH",
         "1:00 PM": None,
+        "1:30 PM": None,
         "2:00 PM": None,
+        "2:30 PM": None,
         "3:00 PM": None,
-        "4:00 PM": None
+        "3:30 PM": None,
+        "4:00 PM": None,
+        "4:30 PM": None
     },
     "Wednesday, February 25th": {
         "8:00 AM": None,
+        "8:30 AM": None,
         "9:00 AM": None,
+        "9:30 AM": None,
         "10:00 AM": None,
-        "11:00 AM": None
+        "10:30 AM": None,
+        "11:00 AM": None,
+        "11:30 AM": None
     }
+}
+
+
+
+blocks = {
+    "Tuesday, February 24th": {
+        "9:00 AM": ["Norton"],
+        "9:30 AM": ["Kimberly-Clark"],   
+        "10:00 AM": ["Kimberly-Clark"],
+        "10:30 AM": ["Apex Tool Group"], 
+        "11:00 AM": ["Kennametal"],
+        "11:30 AM": ["Kennametal"],
+        "1:00 PM": ["Parker Hannifin"],
+        "1:30 PM": ["3M"],                    
+        "2:00 PM": ["3M"],
+        "2:30 PM": ["Sandvik Coromant"],     
+        "3:00 PM": ["Milwaukee"],    
+        "3:30 PM": ["Milwaukee"],
+        "4:00 PM": ["OSG"]
+    },
+    "Wednesday, February 25th": {
+        "9:00 AM": ["Ansell"],
+        "9:30 AM": ["Mitutoyo"],
+        "10:00 AM": ["Mitutoyo"],
+        "10:30 AM": ["Master Fluid Solutions"]
+    }
+}
+
+
+act_session_times = {
+    "Norton": "9:00 AM",
+    "Kimberly-Clark": "9:45 AM",
+    "Apex Tool Group": "10:30 AM",
+    "Kennametal": "11:15 AM",
+    "Parker Hannifin": "1:00",
+    "3M": "1:45 PM",
+    "Sandvik Coromant": "2:30 PM",
+    "Milwaukee": "3:15 PM",
+    "OSG": "4:00 PM",
+    "Ansell": "9:00 AM",
+    "Mitutoyo": "9:45 AM",
+    "Master Fluid Solutions": "10:30 AM"
 }
